de7c8c489690970fc6516fb78e328c975312a027
fbettega <francois.bettega@gmail.com>
2024-10-29 07:16:00 +0100

    initial commit

---
+++ genrating_data.R
@@ -0,0 +1,98 @@
+## parsing deck
+#source : https://scryfall.com/docs/api/bulk-data
+library(plyr)
+library(dplyr)
+library(tidyverse)
+library(scryr)
+# library("ndjson")
+#conflict_prefer("fromJSON", "rjson")
+# mode <- "modern"
+# string <- "Oracle Cards"
+
+DL_scry_fall_json <- function(string){
+options(timeout = 1000)
+  
+files <- scry_bulk_files()
+# json <- httr::GET(files$download_uri[files$name == string])
+
+download.file(files$download_uri[files$name == string],
+              destfile = paste0(
+                "./data/data_",str_replace(string,"\\s+","_"),".json"
+                ),
+              # cacheOK = FALSE,
+              quiet = TRUE,
+              mode="wb")
+
+options(timeout = 60)
+
+data_frame_res <- jsonlite::fromJSON(
+  paste0("./data/data_",str_replace(string,"\\s+","_"),".json")#,  flatten = TRUE
+  ) %>%
+  select(where(~all(!is.na(.))))
+
+
+# unlist(paste0("./data/data_",str_replace(string,"\\s+","_"),".json"))
+
+return(data_frame_res)
+}
+
+
+
+pull_scry_fall <- function(mode){
+  tictoc::tic(mode)
+  if (mode == "oracle") {
+  base_data <- DL_scry_fall_json(
+    "Oracle Cards"
+   # "Default Cards"
+  ) 
+  base_data_rename <- base_data %>%
+    rename_all(~make.unique(colnames(base_data))) %>% 
+    as_tibble()
+  
+  rm(base_data)
+  } else if(mode == "all") {
+    base_data <- DL_scry_fall_json(#scry_bulk_file(
+      "All Cards"
+    ) 
+    
+    base_data_rename <- base_data %>%
+      rename_all(~make.unique(colnames(base_data))) %>% 
+      as_tibble()
+    
+    rm(base_data)
+  }
+  unnest_column_name <- base_data_rename  %>%
+    select(where(is.list)) %>% colnames()
+  
+  
+  unnest_table_col <- map_dfc(unnest_column_name,
+                              ~ unnest_wider(base_data_rename[.x], .x, names_sep = "."))
+  
+  complete_table <- cbind(
+    base_data_rename %>% 
+      select(-all_of(unnest_column_name)),
+    unnest_table_col
+    )
+
+  if (mode == "oracle") {
+    write.csv(
+      complete_table %>%
+                  # filter(legalities.modern == "legal")  %>%
+                  distinct(name,.keep_all = TRUE) %>%
+                  purrr::keep(~!all(is.na(.))),
+              file = "MTG_data/DBcarte_oracle.csv")
+  } else if (mode == "all") {
+    write.csv(complete_table %>%
+                purrr::keep(~!all(is.na(.))) ,
+              file = "MTG_data/DBcarte.csv")
+  }
+  tictoc::toc()
+}
+
+
+
+pull_scry_fall("oracle")
+
+pull_scry_fall("all")
+
+
